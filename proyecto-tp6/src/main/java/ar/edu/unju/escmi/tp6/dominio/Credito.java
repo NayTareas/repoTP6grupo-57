a98873b194b5de221288cfa3778011ac74067d27
Luciana V <166053776+lakslakslak@users.noreply.github.com>
2024-10-12 00:01:45 -0300

    correccion credito

--- proyecto-tp6/src/main/java/ar/edu/unju/escmi/tp6/dominio/Credito.java
+++ proyecto-tp6/src/main/java/ar/edu/unju/escmi/tp6/dominio/Credito.java
@@ -6,72 +6,92 @@ import java.util.List;
 
 public class Credito {
 
-	private TarjetaCredito tarjetaCredito;
-	private Factura factura;
-	private List<Cuota> cuotas = new ArrayList<Cuota>();
+    private TarjetaCredito tarjetaCredito;
+    private Factura factura;
+    private List<Cuota> cuotas = new ArrayList<Cuota>();
 
-	public Credito() {
-	}
+    public Credito() {
+    }
 
-	public Credito(TarjetaCredito tarjetaCredito, Factura factura, List<Cuota> cuotas) {
-	    this.tarjetaCredito = tarjetaCredito;
-	    this.factura = factura;
-	    this.cuotas = cuotas;
-	    generarCuotas(); // Llama a generar las cuotas
-	}
+    public Credito(TarjetaCredito tarjetaCredito, Factura factura, List<Cuota> cuotas) {
+        this.tarjetaCredito = tarjetaCredito;
+        this.factura = factura;
+        this.cuotas = cuotas;
+        generarCuotas(); // Llama a generar las cuotas
+    }
+
+    public TarjetaCredito getTarjetaCredito() {
+        return tarjetaCredito;
+    }
+
+    public void setTarjetaCredito(TarjetaCredito tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public List<Cuota> getCuotas() {
+        return cuotas;
+    }
+
+    public void setCuotas(List<Cuota> cuotas) {
+        this.cuotas = cuotas;
+    }
+
+    public void generarCuotas() {
+        if (!cuotas.isEmpty()) {
+            return;
+        }
+
+        if (factura != null) {
+            double montoPorCuota = factura.calcularTotal() / 30;
+            LocalDate fechaInicial = LocalDate.now();
+            for (int i = 1; i <= 30; i++) {
+                Cuota cuota = new Cuota();
+                cuota.setMonto(montoPorCuota);
+                cuota.setNroCuota(i);
+                cuota.setFechaGeneracion(fechaInicial);
+                cuota.setFechaVencimiento(fechaInicial.plusMonths(i));
+                cuotas.add(cuota);
+            }
+        }
+    }
 
 
-	public Credito(List<Cuota> cuotas) {
-		this.cuotas = cuotas;
-	}
 
-	public TarjetaCredito getTarjetaCredito() {
-		return tarjetaCredito;
-	}
+    // Método para calcular el monto total del crédito
+    public double getMontoTotal() {
+        double total = 0;
+        for (Cuota cuota : cuotas) {
+            total += cuota.getMonto();
+        }
+        return total;
+    }
 
-	public void setTarjetaCredito(TarjetaCredito tarjetaCredito) {
-		this.tarjetaCredito = tarjetaCredito;
-	}
+    public void mostrarCredito() {
+        System.out.println("Tarjeta De Credito: " + tarjetaCredito + "\nFactura: " + factura + "\nCant. Cuotas:\n");
+        for (Cuota cuota : cuotas) {
+            System.out.println(cuota);
+        }
+    }
+    public void setMonto(double montoTotal) {
+        cuotas.clear(); // Limpiamos las cuotas previas si existían
+        double montoPorCuota = montoTotal / 30;
+        LocalDate fechaInicial = LocalDate.now();
+        for (int i = 1; i <= 30; i++) {
+            Cuota cuota = new Cuota();
+            cuota.setMonto(montoPorCuota);
+            cuota.setNroCuota(i);
+            cuota.setFechaGeneracion(fechaInicial);
+            cuota.setFechaVencimiento(fechaInicial.plusMonths(i));
+            cuotas.add(cuota);
+        }
+    }
 
-	public Factura getFactura() {
-		return factura;
-	}
-
-	public void setFactura(Factura factura) {
-		this.factura = factura;
-	}
-
-	public List<Cuota> getCuotas() {
-		return cuotas;
-	}
-
-	public void setCuotas(List<Cuota> cuotas) {
-		this.cuotas = cuotas;
-	}
-	
-	public void generarCuotas() {
-		double montoCuota = this.factura.calcularTotal() / 30;
-		int nroCuota = 0;
-		LocalDate currentDate = LocalDate.now();
-		LocalDate auxDate = LocalDate.now();
-
-		for (int i = 0; i < 30; i++) {
-			nroCuota++;
-			Cuota cuota = new Cuota();
-			cuota.setMonto(montoCuota);
-			cuota.setNroCuota(nroCuota);
-			cuota.setFechaGeneracion(currentDate); 
-			auxDate = auxDate.plusMonths(1);
-			cuota.setFechaVencimiento(auxDate);
-			cuotas.add(cuota);
-		}
-
-	}
-
-	public void mostarCredito() {
-	    System.out.println("Tarjeta De Credito: " + tarjetaCredito + "\nFactura: " + factura + "\nCant. Cuotas:\n");
-	    for (Cuota cuota : cuotas) {
-	        System.out.println(cuota);
-	    }
-	}
 }
